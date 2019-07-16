f222103e0082c2236c5290ac27e670b405dba8c4
rael <rael06@hotmail.fr>
2019-07-16 09:41:09 +0200

    1/4 done

--- demo/src/Controller/BlogController.php
+++ demo/src/Controller/BlogController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,18 +12,33 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog", name="blog")
      */
-    public function index()
+    public function index(ArticleRepository $repo)
     {
+    	$articles = $repo->findAll();
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+	        'articles' => $articles
         ]);
     }
 
     /**
      * @Route("/", name="home")
-     * @return void
      */
     public function home() {
-        return $this->render('blog/home.html.twig');
+        return $this->render('blog/home.html.twig', [
+        	'title' => "Bienvenue ici les amis",
+	        'age' => 36
+        ]);
+    }
+
+	/**
+	 * @Route("/blog/{id}", name="blog_show")
+	 */
+    public function show (Article $article) {
+
+    	return $this->render('blog/show.html.twig', [
+    		'article' => $article
+	    ]);
     }
 }
